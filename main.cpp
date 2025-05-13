36a3f98b679a5a77c100e15c2d20d68ce7f4a3b5
ShahzaibFareedChagani <shahzaib.chagani0306@gmail.com>
2025-05-13 13:13:45 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,22 @@
 #include "Comment.h"
 #include "Notification.h"
 #include "Relationship.h"
+#include "crow_all.h"
 using namespace std;
 
 int main()
 {
+    crow::SimpleApp app;
+    CROW_ROUTE(app, "/api/submit").methods("POST"_method)([](const crow::request& req){
+        auto json_body = crow::json::load(req.body);
+        if (!json_body)
+            return crow::response(400);
+        std::string username = json_body["username"].s();
+        crow::json::wvalue response;
+        response["message"] = "Hello, " + username + " from C++!";
+        return crow::response{response};
+    });
+    app.port(18080).multithreaded().run();
     User u = User(1, "tempuser", "tempPW", "tempemail@gmail.com");
     Post p = Post(1, "Hello", &u);
     cout << p.getTimestamp();
